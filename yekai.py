77076b08017c195e4fad0e61207960796905ea92
CYKAKAKA <yekaic2@illinois.edu>
2018-12-05 16:47:21 -0600

    trying color changing

--- yekai.py
+++ yekai.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import pylab
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -34,14 +35,14 @@ def mod_pert_random(low, likely, high, confidence=4, samples=10000):
 
 
 def mapping(length, width):
-    '''
+    """
     With designated length and width, this function generate a length x width grid. And define the middle point as
     the location of restaurant.
 
     :param length: The number of nodes vertically
     :param width: The number of nodes horizontally
     :return: A NetworkX 2D gird graph
-    '''
+    """
     G = nx.grid_2d_graph(length, width)
     for edge in list(G.edges):
         low = np.random.randint(1, 10)
@@ -55,16 +56,16 @@ def mapping(length, width):
 def prep_time(order_size):
     """
     Return the preparation time for the order based on its size
-    :param size: size for the order
+    :param order_size: size for the order
     :return: preparation time
     """
-    prep_time = np.random.uniform(0, 10, size=100)
+    preparation_time = np.random.uniform(0, 10, size=100)
     if order_size == "S":
-        return prep_time
+        return preparation_time
     elif order_size == "M":
-        return prep_time * 1.5
+        return preparation_time * 1.5
     else:
-        return prep_time * 2
+        return preparation_time * 2
 
 
 def deliver_wait_time():
@@ -85,50 +86,33 @@ def deliver_wait_time():
     return wait_time
 
 
-def judgement(mapgrid, delivery_point):
+def judgement(mapgrid, new_delivery_point):
     """
     Depend on the time that cost on wait and traffic to new delivery point, this function tells if we should wait or
     leave now
 
     :param mapgrid: Made grid with restaurant location
-    :param delivery_point: Randomly generated location on grid
+    :param new_delivery_point: Randomly generated location on grid
     :return: 'W'ait or leave 'N'ow
     """
-    for location, name in nx.get_node_attributes(mapgrid, 'name').items():
+    previous_order_location = (1, 1)
+    coordinate = (0, 0)
+    for restaurant_location, name in nx.get_node_attributes(mapgrid, 'name').items():
         if name == 'RESTAURANT':
-            coordinate = location
-    time = nx.dijkstra_path_length(mapgrid, source=coordinate, target=delivery_point)
-    if time * 2 > deliver_wait_time():
-        return {delivery_point: 'W'}  # for 'wait'
+            coordinate = restaurant_location
+    new_time = nx.dijkstra_path_length(mapgrid, source=coordinate, target=new_delivery_point)
+    previous_time = nx.dijkstra_path_length(mapgrid, source=coordinate, target=previous_order_location)
+    time_two_destination = nx.dijkstra_path_length(mapgrid, source=previous_order_location, target=new_delivery_point)
+    if (new_time + previous_time) * 2 > deliver_wait_time() + new_time + time_two_destination + previous_time:
+        return {new_delivery_point: 'W'}  # for 'wait'
     else:
-        return {delivery_point: 'N'}  # for 'now'
+        return {new_delivery_point: 'N'}  # for 'now'
 
 
 if __name__ == '__main__':
-    new_map = mapping(100, 100)
-    print((judgement(new_map, (2, 3))))
+    new_map = mapping(3, 3)
+    # print(nx.spring_layout(new_map))
+    nx.draw(new_map, node_color=['b', 'r', 'r', 'r', 'b', 'r', 'r', 'r', 'r'])
+    # nx.draw(new_map, pos={(0, 0): np.array([-0.22454824,  0.04780557])}, node_color='b')
+    plt.show(new_map)
 
-# G = nx.DiGraph()
-#
-# for i in range(0, np.size(col) + 1):
-#     G.add_node(i)
-# print('在网络中添加带权中的边...')
-# for i in range(np.size(row)):
-#     G.add_weighted_edges_from([(row[i], col[i], value[i])])
-#
-# print('给网路设置布局...')
-# pos = nx.shell_layout(G)
-# print('画出网络图像：')
-# nx.draw(G, pos, with_labels=True, node_color='white', edge_color='red', node_size=400, alpha=0.5)
-# pylab.title('Self_Define Net', fontsize=15)
-# pylab.show()
-#
-# '''
-# Shortest Path with dijkstra_path
-# '''
-# print('dijkstra方法寻找最短路径：')
-# path = nx.dijkstra_path(G, source=0, target=7)
-# print('节点0到7的路径：', path)
-# print('dijkstra方法寻找最短距离：')
-# distance = nx.dijkstra_path_length(G, source=0, target=7)
-# print('节点0到7的距离为：', distance)
